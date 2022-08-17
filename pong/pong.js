75ace3893e20819ca37ab3c42fa33edf5f6d112e
MateuszPuto <mateuszmputo@gmail.com>
2022-08-17 12:59:19 +0200

    Pong v1

--- pong/pong.js
+++ pong/pong.js
@@ -10,12 +10,15 @@ var paddleDy = 10;
 
 var paddleOneY = middleHeight - 0.5 * paddleSize;
 var paddleTwoY = middleHeight - 0.5 * paddleSize;
+var paddleOneDir = 0;
+var paddleTwoDir = 0;
 
 var ballX = middleWidth;
 var ballY = middleHeight;
 var ballRadius = 10;
-var ballDx = 0.5;
-var ballDy = 0.5;
+var ballDx = 1;
+var ballDy = 1;
+var ballSpeed = 1;
 
 var playerOneScore = 0;
 var playerTwoScore = 0;
@@ -45,7 +48,7 @@ function drawBall(x, y, radius) {
 }
 
 function drawScore() {
-	ctx.font = "48px serif";
+	ctx.font = "48px sans-serif";
 	ctx.fillText(playerOneScore.toString(), middleWidth - 40, 40);
 	ctx.fillText(playerTwoScore.toString(), middleWidth + 10, 40);
 }
@@ -58,7 +61,7 @@ function draw() {
 	clearScrean();
 
 	moveBall();
-	console.log(ballX, ballY);
+	console.log(ballX, ballY, ballSpeed, ballDx, ballDy);
 
 	drawDashedLine();
 	drawPaddle(0, paddleOneY);
@@ -70,7 +73,7 @@ function draw() {
 function moveBall() {
 	ballX += ballDx;
 	ballY += ballDy;
-
+	
 	collisionDetection();
 }
 
@@ -79,32 +82,64 @@ function collisionDetection() {
 		ballDy *= -1;
 	}
 	else if(ballX < ballRadius && ballOnPaddle()) {
-		ballDx *= -1;
-		ballDy = -1 * ballDy;
+		ballDy = -1 * (ballDy + paddleOneDir);
+		ballDx = -1 * Math.sign(ballDx) * Math.sqrt(Math.abs(ballDy)**2 + ballSpeed**2);
+		vec = Math.sqrt(Math.abs(ballDx)**2 + Math.abs(ballDy)**2);
+		
+		ballDx = ballDx * (ballSpeed / vec);
+		ballDy = ballDy * (ballSpeed / vec);
+		
+		ballSpeed *= 1.05;
 	}
 	else if(ballX > canvas.width - ballRadius && ballOnPaddle()) {
-		ballDx *= -1;
-		ballDy = -1 * ballDy;
+		ballDy = -1 * (ballDy + paddleTwoDir);
+		ballDx = -1 * Math.sign(ballDx) * Math.sqrt(Math.abs(ballDy)**2 + ballSpeed**2);
+		vec = Math.sqrt(Math.abs(ballDx)**2 + Math.abs(ballDy)**2);
+		
+		ballDx = ballDx * (ballSpeed / vec);
+		ballDy = ballDy * (ballSpeed / vec);
+	
+		ballSpeed *= 1.05;
 	}
 	else if(ballX < ballRadius) {
 		playerTwoScore += 1;
+	
+		if(playerTwoScore >= 9) {
+			alert("Right side player wins, congratulations!");
+			document.location.reload();
+		}
+
+		ballSpeed = 1;
 		ballX = middleWidth;
 		ballY = middleHeight;
+
+		ballDx *= -1;
+		ballDy *= -1;
 	}
 	else if(ballX > canvas.width - ballRadius) {
 		playerOneScore += 1;
+
+		if(playerOneScore >= 9) {
+			alert("Left side player wins, congratulations!");
+			document.location.reload();
+		}
+
+		ballSpeed = 1;
 		ballX = middleWidth;
 		ballY = middleHeight;
+
+		ballDx *= -1;
+		ballDy *= -1;
 	}
 }
 
 function ballOnPaddle() {
 	let hit = false;
 
-	if(ballY > paddleOneY && ballY < paddleOneY + paddleSize) {
+	if(ballY - 2 > paddleOneY && ballY + 2 < paddleOneY + paddleSize) {
 		hit = true;
 	}
-	else if(ballY > paddleTwoY && ballY < paddleTwoY + paddleSize) {
+	else if(ballY - 2 > paddleTwoY && ballY + 2 < paddleTwoY + paddleSize) {
 		hit = true;
 	}
 
@@ -114,24 +149,44 @@ function ballOnPaddle() {
 function keyDownHandler(e) {
 	switch (e.key) {
 		case "ArrowDown":
-			paddleTwoY += paddleDy;
+			if(paddleTwoY + paddleSize < canvas.height) {
+				paddleTwoY += paddleDy;
+			}
+			paddleTwoDir = 1;
 			break;
 		case "ArrowUp":
-			paddleTwoY -= paddleDy;
+			if(paddleTwoY > 0) {
+				paddleTwoY -= paddleDy;
+			}
+			paddleTwoDir = -1;
 			break;
 		case "s":
-			paddleOneY += paddleDy;
+			if(paddleOneY + paddleSize < canvas.height) {
+				paddleOneY += paddleDy;
+			}
+			paddleOneDir = 1;
 			break;
 		case "w":
-			paddleOneY -= paddleDy;
+			if(paddleOneY > 0) {
+				paddleOneY -= paddleDy;
+			}
+			paddleOneDir = -1;
 			break;
-	
+		default:
+			paddleOneDir = 0;
+			paddleTwoDir = 0;
 	}
+}
+
+function keyUpHandler(e) {
+	paddleOneDir = 0;
+	paddleTwoDir = 0;
 }
 
 function main() {
 	document.addEventListener("keydown", keyDownHandler, false);
-
+	document.addEventListener("keyup", keyUpHandler, false);
+		
 	setInterval(draw, 10);
 }
 
