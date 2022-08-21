d81e1832e769ec1ea57c8ce23a1459d0e754a2e7
MateuszPuto <mateuszmputo@gmail.com>
2022-08-21 18:43:12 +0200

    all aliens

--- space_invaders/space_invaders.js
+++ space_invaders/space_invaders.js
@@ -1,8 +1,39 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-const img = new Image();
-img.onload = () => {
-	ctx.drawImage(img, canvas.width/2, canvas.height/2, 40, 40);
+const imagePaths = [
+	"./assets/alien-1.png",
+	"./assets/alien-1-pose-2.png",
+	"./assets/alien-2.png",
+	"./assets/alien-2-pose-2.png",
+	"./assets/alien-3.png",
+	"./assets/alien-3-pose-2.png"
+];
+
+const sprites = [];
+for(let i=0; i<imagePaths.length; i++) {
+	createImage(sprites, "myCanvas");
 }
-	img.src = "alien-1.png";
+
+
+let x = 10;
+let y = 100;
+for (let i=0; i<sprites.length; i++) {
+	sprites[i].onload = loadImage(sprites[i], x, y, 40, 40);
+	x += 50;
+}
+
+for (let i=0; i<sprites.length; i++) {
+	sprites[i].src = imagePaths[i];
+}
+
+function createImage(sprites, canvas) {
+	image = new Image();
+	document.getElementById(canvas).appendChild(image);
+	sprites.push(image);
+}
+
+function loadImage(image, posX, posY, sizeX, sizeY) {
+	return () => {ctx.drawImage(image, posX, posY, sizeX, sizeY);}
+}
+
